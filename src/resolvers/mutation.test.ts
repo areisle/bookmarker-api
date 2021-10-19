2e0937a55a3f942d76757837a1ded3b51e9c8130
areisle <areisle@gmail.com>
2021-10-18 21:50:31 -0700

    test: finish more auth tests

--- src/resolvers/mutation.test.ts
+++ src/resolvers/mutation.test.ts
@@ -221,7 +221,6 @@ describe("user doesn't belong to category", () => {
             },
         }, mockRequest(userTokens[0]));
 
-        console.log(response)
         expect(response.errors).toHaveLength(1)
         expect(response.errors?.[0].extensions?.code).toEqual('FORBIDDEN')
     });
@@ -246,12 +245,175 @@ describe("user doesn't belong to category", () => {
 });
 
 describe("user is invited to category", () => {
-    test.todo("user can't get category");
-    test.todo("user can't add bookmarks");
-    test.todo("user can't remove bookmarks");
-    test.todo("user can't add tags");
-    test.todo("user can't remove tags");
-    test.todo("user can't add users");
+    let categoryId: number;
+    let bookmarkId: number;
+
+    beforeAll(async () => {
+        // add category for user 2
+        let response = await server.executeOperation({
+            query: gql`
+                mutation addCategory($name: String!) {
+                    addCategory(name: $name) {
+                        id
+                        name
+                    }
+                }
+            `,
+            variables: {
+                name: 'User 2s Private Category'
+            },
+        }, mockRequest(userTokens[1]));
+        categoryId = response.data?.addCategory.id
+
+        // add user
+        response = await server.executeOperation({
+            query: gql`
+                mutation addUsers($categoryId: Int!, $emails: [String!]!) {
+                    addUsers(categoryId: $categoryId, emails: $emails)
+                }
+            `,
+            variables: {
+                categoryId,
+                emails: [`test-user-1@email.ca`]
+            },
+        }, mockRequest(userTokens[1]));
+
+
+        // add a bookmark
+        response = await server.executeOperation({
+            query: gql`
+                mutation addBookmark($input: CreateBookmarkContent!) {
+                    addBookmark(input: $input) {
+                        id
+                    }
+                }
+            `,
+            variables: {
+                input: {
+                    categoryId,
+                    title: 'something',
+                    url: 'https://test.com/something',
+                    description: '',
+                    tags: ['hello', 'world']
+                }
+            },
+        }, mockRequest(userTokens[1]));
+
+        bookmarkId = response.data?.addBookmark.id
+    })
+
+    test("user can't get category", async () => {
+        const response = await server.executeOperation({
+            query: gql`
+                query getCategory($id: Int!) {
+                    category(id: $id) {
+                        name
+                    }
+                }
+            `,
+            variables: {
+                id: categoryId
+            },
+        }, mockRequest(userTokens[0]));
+
+        expect(response.data?.category).toBeNull()
+        expect(response.errors).toHaveLength(1)
+        expect(response.errors?.[0].extensions?.code).toEqual('FORBIDDEN')
+    });
+
+    test("user can't add bookmarks", async () => {
+        const response = await server.executeOperation({
+            query: gql`
+                mutation addBookmark($input: CreateBookmarkContent!) {
+                    addBookmark(input: $input) {
+                        id
+                    }
+                }
+            `,
+            variables: {
+                input: {
+                    categoryId,
+                    title: 'something',
+                    url: 'https://test.com/something',
+                    description: '',
+                    tags: ['hello', 'world']
+                }
+            },
+        }, mockRequest(userTokens[0]));
+
+        expect(response.data?.addBookmark).toBeNull()
+        expect(response.errors).toHaveLength(1)
+        expect(response.errors?.[0].extensions?.code).toEqual('FORBIDDEN')
+    });
+
+    test("user can't remove bookmarks", async () => {
+        let response = await server.executeOperation({
+            query: gql`
+                mutation removeBookmark($id: Int!) {
+                    removeBookmark(id: $id)
+                }
+            `,
+            variables: {
+                id: bookmarkId
+            },
+        }, mockRequest(userTokens[0]));
+
+        expect(response.errors).toHaveLength(1)
+        expect(response.errors?.[0].extensions?.code).toEqual('FORBIDDEN')
+    });
+
+    test("user can't add tags", async () => {
+        let response = await server.executeOperation({
+            query: gql`
+                mutation addTag($bookmarkId: Int!, $name: String!) {
+                    addTag(bookmarkId: $bookmarkId, name: $name)
+                }
+            `,
+            variables: {
+                bookmarkId,
+                name: 'new tag'
+            },
+        }, mockRequest(userTokens[0]));
+
+        expect(response.errors).toHaveLength(1)
+        expect(response.errors?.[0].extensions?.code).toEqual('FORBIDDEN')
+    });
+
+    test("user can't remove tags", async () => {
+        let response = await server.executeOperation({
+            query: gql`
+                mutation removeTag($bookmarkId: Int!, $name: String!) {
+                    removeTag(bookmarkId: $bookmarkId, name: $name)
+                }
+            `,
+            variables: {
+                bookmarkId,
+                name: 'hello'
+            },
+        }, mockRequest(userTokens[0]));
+
+        expect(response.errors).toHaveLength(1)
+        expect(response.errors?.[0].extensions?.code).toEqual('FORBIDDEN')
+    });
+
+    test("user can't invite users", async () => {
+        let response = await server.executeOperation({
+            query: gql`
+                mutation addUsers($categoryId: Int!, $emails: [String!]!) {
+                    addUsers(categoryId: $categoryId, emails: $emails)
+                }
+            `,
+            variables: {
+                categoryId,
+                emails: [`test-user-3@email.ca`]
+            },
+        }, mockRequest(userTokens[0]));
+
+        expect(response.errors).toHaveLength(1)
+        expect(response.errors?.[0].extensions?.code).toEqual('FORBIDDEN')
+
+    });
+
     test.todo("user can reject invitation to join category");
     test.todo("user can accept invitation to join category");
 });
